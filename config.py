b51d1a27f9c48c6653ec47a27296ff68f8d11d17
tanvir-alam-sk <tanviralamsk47@gmail.com>
2024-12-31 16:51:39 +0600

    initial

---
+++ config.py
@@ -0,0 +1,7 @@
+DB_USERNAME='tanvir'
+DB_PASSWORD='tanvir1234'
+DB_NAME='scrapingdb1'
+DB_PORT='5432'
+DB_HOST='postgres'
+# OLLAMA_HOST='http://ollama:11434'
+SECRET_KEY='django-insecure-nc4($e*vaa^7ftbpg^5y8yz-5a(-n18-*#ln^wpbtw5a0-@e5('
